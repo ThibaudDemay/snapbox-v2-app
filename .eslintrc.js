6d941d9b499ca336290c5e9c8691e56ca9452256
Thibaud Demay <demay.thibaud@gmail.com>
2021-08-28 13:21:17 +0200

    Update conf to match with beardedbear conf (FTW)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
@@ -16,5 +16,20 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "no-unused-vars": "off",
+    "vue/script-setup-uses-vars": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "vue/no-unused-properties": [
+      "warn",
+      {
+        groups: ["props", "setup"],
+        deepData: true,
+      },
+    ],
   },
 };
