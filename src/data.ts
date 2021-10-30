0c1cbea587ab614dc6916e3191195a34f05655eb
Thibaud Demay <demay.thibaud@gmail.com>
2021-10-30 22:22:41 +0200

    update data for fixtures

--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,31 @@
-const data = {
-  tables: [{}, { text: "Honneur", value: "honneur" }],
+interface GenData {
+  text: string;
+  value: string;
+}
+
+interface Data {
+  tables: Array<GenData>;
+  names: Record<string, Array<GenData>>;
+}
+
+const data: Data = {
+  tables: [
+    { text: "Honneur", value: "honneur" },
+    { text: "Girafe", value: "girafe" },
+    { text: "Capybara", value: "capybara" },
+  ],
   names: {
     honneur: [
-      {},
       { text: "Le Marié", value: "le-marie" },
       { text: "La Mariée", value: "la-mariee" },
+    ],
+    girafe: [
+      { text: "Paul", value: "paul" },
+      { text: "Jocelyne", value: "jocelyne" },
+    ],
+    capybara: [
+      { text: "Kevin", value: "kevin" },
+      { text: "Sandra", value: "sandra" },
     ],
   },
 };
